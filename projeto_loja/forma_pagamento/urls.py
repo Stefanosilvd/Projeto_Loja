96201657623fa044eb3d6325600d14f8f67e30cc
CelinaSantos1 <celinamariasilva296@gmail.com>
2025-07-09 20:08:02 +0000

    salvo1

--- projeto_loja/forma_pagamento/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import pagamento_view
-
-urlpatterns = [
-    path('', pagamento_view, name='pagamento'),
-]
--- projeto_loja/forma_pagamento/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def pagamento_view(request):
-    return HttpResponse("Página de forma de pagamento")
